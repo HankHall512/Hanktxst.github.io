0b738e732df0d96887be2d7553b715d06c8efe36
Hank Hall <hankfuereone@gmail.com>
2025-10-02 14:33:19 -0500

    Executed the welcome function in external.js from an internal script.

--- javascript/external.js
+++ javascript/external.js
@@ -1,15 +1,36 @@
-<!--document.write("Where is my JS code?");-->
-<!--window.alert(5 + 6);-->
-<!--window.alert('5 + 6');-->
-<!--document.write('<h1>A heading</h1>');document.write('<p>A sentence.</p>');-->
-<!--const x = "web"; const y = "mapping"; const out = x + y; document.write(out);-->
-<!--// A prompt box is used to prompt users to input a value before entering a page.user name = window.prompt("Please enter your name", "Type your name here");document.write(user name);-->
-a = 22; const b = 33;
-document.writeln(a + b);
-document.writeln("<br>");
-document.writeln(a += b);
-document.writeln("<br>");
-document.writeln(a += b);
 /*
 window.onload = function() {
+  // Prompt the user for their name
+  let userName = prompt("Please enter your name:");
+
+  // Update the header with the user's name
+  if (userName) {
+    document.getElementById("header-greeting").textContent = "Hello, welcome " + userName + "!";
+  } else {
+    document.getElementById("header-greeting").textContent = "Hello, welcome Guest!";
+  }
+
+  // Optional: Add your conditional test button back in
+  let button = document.createElement("button");
+  button.textContent = "Conditional Test";
+  button.onclick = function() {
+    let x = confirm("Are you sure you want to proceed?");
+    if (x) {
+      alert("You chose Okay!");
+    } else {
+      alert("You chose Cancel!");
+    }
+  };
+  document.body.appendChild(button);
+};
+*/
+function welcome()
+{
+    let a = "Please enter your name.";
+    let b = "Type your name here.";
+// A prompt box is used to prompt users to input a value before entering a page.
+    user_name = window.prompt(a, b);
+    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+    return message
+}
 document.write(welcome());
